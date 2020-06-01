73eeb26c1adcd89872506b893a07c97594f83b0a
Lucas Werkmeister <mail@lucaswerkmeister.de>
2020-06-01 19:21:54 +0200

    Add ZReference runtime object
    Instead of immediately evaluating a reference node, make it return a
    runtime object which can be evaluated on-demand; for interop, this is
    exposed as a function with no arguments.
    
    ZReference is not added to ZTypes because, judging from SimpleLanguage,
    it doesn’t look like all runtime types have to be registered there. (I’m
    not sure what the rules for this are, but for now, not registering
    ZReference keeps things simpler.)

--- language/src/main/java/de/lucaswerkmeister/graaleneyj/nodes/ZReferenceLiteralNode.java
+++ language/src/main/java/de/lucaswerkmeister/graaleneyj/nodes/ZReferenceLiteralNode.java
@@ -1,24 +1,15 @@
 package de.lucaswerkmeister.graaleneyj.nodes;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.UnexpectedResultException;
 
 import de.lucaswerkmeister.graaleneyj.ZConstants;
-import de.lucaswerkmeister.graaleneyj.parser.ZCanonicalJsonParser;
 import de.lucaswerkmeister.graaleneyj.runtime.ZList;
+import de.lucaswerkmeister.graaleneyj.runtime.ZReference;
 
 public class ZReferenceLiteralNode extends ZNode {
 
 	private final String id;
-	private static final Map<String, Object> registry = new HashMap<>();
 
 	public ZReferenceLiteralNode(String id) {
 		this.id = id;
@@ -45,22 +36,8 @@ public class ZReferenceLiteralNode extends ZNode {
 			return false;
 		case ZConstants.NIL:
 			return ZList.NIL;
-		}
-
-		// TODO for now, global scope is all we have
-		if (registry.containsKey(id)) {
-			return registry.get(id);
-		} else {
-			CompilerDirectives.transferToInterpreter();
-			try (FileReader file = new FileReader("abstracttext/eneyj/data/" + id + ".json")) {
-				JsonElement element = new Gson().fromJson(file, JsonElement.class);
-				ZNode node = ZCanonicalJsonParser.parseJsonElement(element);
-				Object value = node.execute(virtualFrame);
-				registry.put(id, value);
-				return value;
-			} catch (IOException e) {
-				throw new RuntimeException(e);
-			}
+		default:
+			return new ZReference(id);
 		}
 	}
 
---
+++ language/src/main/java/de/lucaswerkmeister/graaleneyj/runtime/ZReference.java
@@ -0,0 +1,69 @@
+package de.lucaswerkmeister.graaleneyj.runtime;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.interop.ArityException;
+import com.oracle.truffle.api.interop.InteropLibrary;
+import com.oracle.truffle.api.interop.TruffleObject;
+import com.oracle.truffle.api.library.ExportLibrary;
+import com.oracle.truffle.api.library.ExportMessage;
+
+import de.lucaswerkmeister.graaleneyj.nodes.ZNode;
+import de.lucaswerkmeister.graaleneyj.parser.ZCanonicalJsonParser;
+
+/**
+ * An unevaluated reference, exposed to other languages as a 0-adic function;
+ * calling the function evaluates the reference.
+ */
+@ExportLibrary(InteropLibrary.class)
+public class ZReference implements TruffleObject {
+
+	private final String id;
+	private static final Map<String, Object> registry = new HashMap<>();
+
+	public ZReference(String id) {
+		this.id = id;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	@ExportMessage
+	public boolean isExecutable() {
+		return true;
+	}
+
+	@ExportMessage
+	public Object execute(Object... arguments) throws ArityException {
+		if (arguments.length > 0) {
+			throw ArityException.create(0, arguments.length);
+		}
+		return evaluate();
+	}
+
+	public Object evaluate() {
+		// TODO for now, global scope is all we have
+		if (registry.containsKey(id)) {
+			return registry.get(id);
+		} else {
+			CompilerDirectives.transferToInterpreter();
+			try (FileReader file = new FileReader("abstracttext/eneyj/data/" + id + ".json")) {
+				JsonElement element = new Gson().fromJson(file, JsonElement.class);
+				ZNode node = ZCanonicalJsonParser.parseJsonElement(element);
+				Object value = node.execute(null); // TODO where do we get a virtualFrame from?
+				registry.put(id, value);
+				return value;
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+}
