1476c03f3d941ead651b586f8ec2457e83f0e4ee
Lucas Werkmeister <mail@lucaswerkmeister.de>
2020-08-02 17:53:59 +0200

    Restore and update old ZTypes code
    This code was commented out (originally coming from SimpleLanguage) back
    when I still thought Z13/nil should be the “null” value for interop, but
    in fact that role should be filled by Z23/nothing. This means that we
    can use the code again, though it’s not actually used for anything at
    the moment, since we have no @Specialization for ZNothing.

--- language/src/main/java/de/lucaswerkmeister/graaleneyj/nodes/ZTypes.java
+++ language/src/main/java/de/lucaswerkmeister/graaleneyj/nodes/ZTypes.java
@@ -1,10 +1,13 @@
 package de.lucaswerkmeister.graaleneyj.nodes;
 
 import com.oracle.truffle.api.dsl.ImplicitCast;
+import com.oracle.truffle.api.dsl.TypeCast;
+import com.oracle.truffle.api.dsl.TypeCheck;
 import com.oracle.truffle.api.dsl.TypeSystem;
 
 import de.lucaswerkmeister.graaleneyj.runtime.ZCharacter;
 import de.lucaswerkmeister.graaleneyj.runtime.ZList;
+import de.lucaswerkmeister.graaleneyj.runtime.ZNothing;
 import de.lucaswerkmeister.graaleneyj.runtime.ZObject;
 
 @TypeSystem({
@@ -22,27 +25,26 @@ import de.lucaswerkmeister.graaleneyj.runtime.ZObject;
 		ZObject.class })
 public abstract class ZTypes {
 
-// Commented out because there currently is no such thing as a ZNil type
-//	/**
-//	 * Manually implemented type check replacing the Truffle-generated one:
-//	 * as there is only a {@link ZNil#SINGLETON singleton} instance,
-//	 * we can skip an {@code instanceof} check.
-//	 */
-//	@TypeCheck(ZNil.class)
-//	public static boolean isZNil(Object value) {
-//		return value == ZNil.SINGLETON;
-//	}
-//
-//	/**
-//	 * Manually implemented type cast replacing the Truffle-generated one:
-//	 * as there is only a {@link ZNil#SINGLETON singleton} instance,
-//	 * we can skip an actual cast.
-//	 */
-//	@TypeCast(ZNil.class)
-//	public static ZNil asZNil(Object value) {
-//		assert isZNil(value);
-//		return ZNil.SINGLETON;
-//	}
+	/**
+	 * Manually implemented type check replacing the Truffle-generated one: as there
+	 * is only a {@link ZNothing#SINGLETON singleton} instance, we can skip an
+	 * {@code instanceof} check.
+	 */
+	@TypeCheck(ZNothing.class)
+	public static boolean isZNothing(Object value) {
+		return value == ZNothing.INSTANCE;
+	}
+
+	/**
+	 * Manually implemented type cast replacing the Truffle-generated one: as there
+	 * is only a {@link ZNothing#SINGLETON singleton} instance, we can skip an
+	 * actual cast.
+	 */
+	@TypeCast(ZNothing.class)
+	public static ZNothing asZNothing(Object value) {
+		assert isZNothing(value);
+		return ZNothing.INSTANCE;
+	}
 
 	@ImplicitCast
 	public static ZCharacter intToZCharacter(int character) {
