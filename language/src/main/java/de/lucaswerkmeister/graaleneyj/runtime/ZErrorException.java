61b213665d7b19eb0efd0e2d298906f9f9259081
Lucas Werkmeister <mail@lucaswerkmeister.de>
2021-01-25 23:14:35 +0100

    Migrate from TruffleException to AbstractTruffleException
    I have a feeling the error object we delegate to should eventually be a
    special kind of object with some extra interop messages (so I guess
    ZObjectLiteralNode should look at the type and instantiate a different
    class?), but for now a ZObject should do. All tests pass, at least.
    
    Fixes #4.

--- language/src/main/java/de/lucaswerkmeister/graaleneyj/runtime/ZErrorException.java
+++ language/src/main/java/de/lucaswerkmeister/graaleneyj/runtime/ZErrorException.java
@@ -1,37 +1,36 @@
 package de.lucaswerkmeister.graaleneyj.runtime;
 
-import com.oracle.truffle.api.TruffleException;
+import com.oracle.truffle.api.exception.AbstractTruffleException;
+import com.oracle.truffle.api.interop.ExceptionType;
+import com.oracle.truffle.api.interop.InteropLibrary;
 import com.oracle.truffle.api.interop.TruffleObject;
+import com.oracle.truffle.api.library.ExportLibrary;
+import com.oracle.truffle.api.library.ExportMessage;
 import com.oracle.truffle.api.nodes.Node;
 
 /**
  * An exception wrapping an uncaught eneyj error object.
  */
-public class ZErrorException extends RuntimeException implements TruffleException {
+@ExportLibrary(value = InteropLibrary.class, delegateTo = "error")
+public class ZErrorException extends AbstractTruffleException {
 
 	private static final long serialVersionUID = 1L;
-	private final TruffleObject error;
-	private final Node location;
+	protected final TruffleObject error;
 
 	public ZErrorException(TruffleObject error, Node location) {
+		super(location);
 		// TODO assert error != null;
 		this.error = error;
-		this.location = location;
 	}
 
-	@Override
-	public synchronized Throwable fillInStackTrace() {
-		return this;
+	@ExportMessage
+	public ExceptionType getExceptionType() {
+		return ExceptionType.RUNTIME_ERROR;
 	}
 
-	@Override
-	public Object getExceptionObject() {
-		return error;
-	}
-
-	@Override
-	public Node getLocation() {
-		return location;
+	@ExportMessage
+	public boolean isExceptionIncompleteSource() {
+		return false;
 	}
 
 }
