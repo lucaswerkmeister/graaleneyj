6bb4d18910cc94158f6ca24280bd2af73276a382
Lucas Werkmeister <mail@lucaswerkmeister.de>
2021-02-24 22:09:42 +0100

    Make ZNothing extend ZObject

--- language/src/main/java/de/lucaswerkmeister/graaleneyj/runtime/ZNothing.java
+++ language/src/main/java/de/lucaswerkmeister/graaleneyj/runtime/ZNothing.java
@@ -1,37 +1,24 @@
 package de.lucaswerkmeister.graaleneyj.runtime;
 
-import com.oracle.truffle.api.TruffleLanguage;
 import com.oracle.truffle.api.interop.InteropLibrary;
-import com.oracle.truffle.api.interop.TruffleObject;
 import com.oracle.truffle.api.library.ExportLibrary;
 import com.oracle.truffle.api.library.ExportMessage;
-
-import de.lucaswerkmeister.graaleneyj.ZLanguage;
 
 /**
  * The Z23 nothing value. For interop, this acts as the null value.
  */
 @ExportLibrary(InteropLibrary.class)
-public class ZNothing implements TruffleObject {
+public class ZNothing extends ZObject {
 
 	public static final ZNothing INSTANCE = new ZNothing();
 
 	private ZNothing() {
+		super(STATIC_BLANK_SHAPE);
 	}
 
 	@ExportMessage
 	public final boolean isNull() {
 		return true;
-	}
-
-	@ExportMessage
-	public final boolean hasLanguage() {
-		return true;
-	}
-
-	@ExportMessage
-	public final Class<? extends TruffleLanguage<?>> getLanguage() {
-		return ZLanguage.class;
 	}
 
 	@ExportMessage
