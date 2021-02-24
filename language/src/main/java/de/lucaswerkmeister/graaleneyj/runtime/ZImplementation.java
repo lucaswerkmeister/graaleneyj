abcd3dcf58f2d078134586e3bfd9011d2b823ed2
Lucas Werkmeister <mail@lucaswerkmeister.de>
2021-02-24 22:12:34 +0100

    Make ZImplementation extend ZObject

--- language/src/main/java/de/lucaswerkmeister/graaleneyj/runtime/ZImplementation.java
+++ language/src/main/java/de/lucaswerkmeister/graaleneyj/runtime/ZImplementation.java
@@ -1,37 +1,24 @@
 package de.lucaswerkmeister.graaleneyj.runtime;
 
 import com.oracle.truffle.api.CallTarget;
-import com.oracle.truffle.api.TruffleLanguage;
 import com.oracle.truffle.api.interop.InteropLibrary;
-import com.oracle.truffle.api.interop.TruffleObject;
 import com.oracle.truffle.api.library.ExportLibrary;
 import com.oracle.truffle.api.library.ExportMessage;
 
-import de.lucaswerkmeister.graaleneyj.ZLanguage;
-
 @ExportLibrary(InteropLibrary.class)
-public class ZImplementation implements TruffleObject {
+public class ZImplementation extends ZObject {
 
 	private final CallTarget callTarget;
 	private final String functionId;
 
 	public ZImplementation(CallTarget callTarget, String functionId) {
+		super(STATIC_BLANK_SHAPE);
 		this.callTarget = callTarget;
 		this.functionId = functionId;
 	}
 
 	public CallTarget getCallTarget() {
 		return callTarget;
-	}
-
-	@ExportMessage
-	public final boolean hasLanguage() {
-		return true;
-	}
-
-	@ExportMessage
-	public final Class<? extends TruffleLanguage<?>> getLanguage() {
-		return ZLanguage.class;
 	}
 
 	@ExportMessage
