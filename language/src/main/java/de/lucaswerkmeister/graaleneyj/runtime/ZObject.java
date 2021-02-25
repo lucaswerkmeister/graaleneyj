992d4ce785f939f413835cc82605af17e8fa9398
Lucas Werkmeister <mail@lucaswerkmeister.de>
2021-02-25 20:26:17 +0100

    Clarify ZObject dynamic object usage
    Explain in the class javadoc who is and isn’t allowed to add dynamic
    members: you’re allowed to add them iff you just created the object and
    supplied its initial shape.

--- language/src/main/java/de/lucaswerkmeister/graaleneyj/runtime/ZObject.java
+++ language/src/main/java/de/lucaswerkmeister/graaleneyj/runtime/ZObject.java
@@ -6,6 +6,7 @@ import com.oracle.truffle.api.interop.TruffleObject;
 import com.oracle.truffle.api.library.ExportLibrary;
 import com.oracle.truffle.api.library.ExportMessage;
 import com.oracle.truffle.api.object.DynamicObject;
+import com.oracle.truffle.api.object.DynamicObjectLibrary;
 import com.oracle.truffle.api.object.Shape;
 
 import de.lucaswerkmeister.graaleneyj.ZLanguage;
@@ -17,6 +18,19 @@ import de.lucaswerkmeister.graaleneyj.ZLanguage;
  * <p>
  * Subclasses must at least export
  * {@link InteropLibrary#toDisplayString(Object, boolean) toDisplayString}.
+ * </p>
+ * <p>
+ * All ZObjects extend {@link DynamicObject}. However, objects are meant to be
+ * immutable, and not all are meant to have any dynamic members. Consequently,
+ * the only place where it is allowed to add dynamic members to a ZObject (using
+ * a {@link DynamicObjectLibrary}) is immediately after the object was created
+ * using a {@link Shape} taken from the {@link ZContext context}. Subclasses
+ * that are not meant to have dynamic members should not have a constructor that
+ * takes a {@link Shape} argument, and should instead pass the
+ * {@link #STATIC_BLANK_SHAPE} into the parent constructor; this signals to
+ * users of the class that they are not allowed to add dynamic members even if
+ * they created the object, because they did not pass a {@link Shape} into the
+ * constructor.
  * </p>
  *
  * @see ZPlainObject
