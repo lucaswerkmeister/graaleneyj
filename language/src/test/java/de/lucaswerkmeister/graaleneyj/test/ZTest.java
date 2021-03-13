50186e7e9368c6e924df07a851c68d60fa1d8262
Lucas Werkmeister <mail@lucaswerkmeister.de>
2021-03-13 18:57:32 +0100

    Simplify ZTest.eval()
    Context has a method taking a language ID and code snippet directly,
    there’s no need for us to build a source. I’m not sure where the old
    code came from – probably SimpleLanguage?
    
    (One difference is that the source name will now be “Unnamed” rather
    than “test”. I think that’s okay.)

--- language/src/test/java/de/lucaswerkmeister/graaleneyj/test/ZTest.java
+++ language/src/test/java/de/lucaswerkmeister/graaleneyj/test/ZTest.java
@@ -3,7 +3,6 @@ package de.lucaswerkmeister.graaleneyj.test;
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.HostAccess;
 import org.graalvm.polyglot.PolyglotAccess;
-import org.graalvm.polyglot.Source;
 import org.graalvm.polyglot.Value;
 import org.junit.After;
 import org.junit.Before;
@@ -31,8 +30,7 @@ public abstract class ZTest {
 	}
 
 	protected Value eval(String code) {
-		Source source = Source.newBuilder(ZLanguage.ID, code, "test").buildLiteral();
-		return context.eval(source);
+		return context.eval(ZLanguage.ID, code);
 	}
 
 }
